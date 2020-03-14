5e2a5e44f0bde486217ef87da051ba1d58404081
yberkof <yahia19998120@gmail.com>
2020-03-14 13:29:41 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-material2-list-of-cards-6njpkv
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-material2-list-of-cards-6njpkv)
---
+++ app/app.component.ts
@@ -0,0 +1,18 @@
+import {Component} from '@angular/core';
+import {VERSION} from '@angular/material';
+
+@Component({
+  selector: 'material-app',
+  templateUrl: 'app.component.html',
+  styleUrls: ['app.component.scss']
+})
+export class AppComponent { 
+  version = VERSION;
+}
+
+/**
+ * Copyright Google LLC All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://angular.io/license
+ */
